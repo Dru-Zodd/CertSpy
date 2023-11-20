a700413fd9a067fabd0ffc8474508614f449c4a3
Dru Banks <lamarsepi0l@localhost.localdomain>
2023-11-19 18:48:39 -0600

    Revised version of certspy. Added termcolor and argparse.

--- CertSpy.py
+++ CertSpy.py
@@ -1,6 +1,8 @@
-import sys
 import requests
 import re
+import argparse
+from termcolor import colored, cprint
+
 
 banner = """
 _________                __   _________             
@@ -8,14 +10,26 @@ _________                __   _________
 /    \  \/_/ __ \_  __ \   __\_____  \\____ <   |  |
 \     \___\  ___/|  | \/|  | /        \  |_> >___  |
  \______  /\___  >__|   |__|/_______  /   __// ____|
-        \/     \/                   \/|__|   \/     
+        \/     \/                   \/|__|   \/    
+
+        Author: Dru Banks(S0KRAT3Z)
+        Company: Blue Cord Security 
+        Version: 1.0.1
+        For Legal and Ethical Use Only
 """
-print(banner)
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--domain', required=True, help='Website to enumerate')
+    arguments = parser.parse_args()
+    return arguments
 
 def get_subdomains(domain):
+    cprint("[+] Getting the l00t...", "green")
     url = f"https://crt.sh/?q=%.{domain}&output=json"
-    response = requests.get(url)
-    if response.ok:
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
         data = response.json()
         subdomains = set()
         for item in data:
@@ -26,23 +40,20 @@ def get_subdomains(domain):
                     if subname.find('*') == -1:
                         subdomains.add(subname)
         return subdomains
-    else:
-        print("Error retrieving data from crt.sh")
+    except requests.RequestException:
+        cprint("[-] Error retrieving data from crt.sh", "red")
         return set()
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python3 CertSpy.py <domain>")
-        sys.exit(1)
-
-    domain = sys.argv[1]
-    subdomains = get_subdomains(domain)
+    print(banner)
+    args = get_args()
+    subdomains = get_subdomains(args.domain)
     if subdomains:
-        print(f"Subdomains of {domain}:")
+        cprint(f"[*] Subdomains of {args.domain}:", "green")
         for subdomain in subdomains:
-            print(subdomain)
+            cprint(subdomain, "yellow")
     else:
-        print(f"No subdomains found for {domain}")
+        cprint(f"[-] No subdomains found for {args.domain}", "red")
 
 if __name__ == "__main__":
     main()
